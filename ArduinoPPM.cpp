5a70450e2e95823bdc1071ea96a1e11f16dc3454
Kruemmelspalter <54735589+Kruemmelspalter@users.noreply.github.com>
2020-01-11 18:48:15 +0100

    Update ArduinoPPM.cpp
    Added debug function

--- ArduinoPPM.cpp
+++ ArduinoPPM.cpp
@@ -9,9 +9,18 @@ void ArduinoPPM::send(int values[]){
     for(int i=0;i<sizeof(values);i++){
         int value=values[i];
         digitalWrite(_pin, LOW);
+        #ifdef debug
+            Serial.out.println(0);
+        #endif
         delayMicroseconds(400);
         digitalWrite(_pin, HIGH);
+        #ifdef debug
+            Serial.out.println(1);
+        #endif
         delayMicroseconds(value);
     }
+    #ifdef debug
+        Serial.out.println("Wait");
+    #endif
     delayMicroseconds(12100);
 }
