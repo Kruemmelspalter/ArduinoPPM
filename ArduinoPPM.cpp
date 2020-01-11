9e6c446cb9151e0b808b17431d107bbd8dc5d44e
Kruemmelspalter <54735589+Kruemmelspalter@users.noreply.github.com>
2020-01-11 19:14:14 +0100

    Update ArduinoPPM.cpp

--- ArduinoPPM.cpp
+++ ArduinoPPM.cpp
@@ -1,26 +1,27 @@
 #include "Arduino.h"
 #include "ArduinoPPM.h"
 
-ArduinoPPM::ArduinoPPM(int pin){
+ArduinoPPM::ArduinoPPM(int pin, bool debug){
     pinMode(pin, OUTPUT);
     _pin = pin;
+    _debug=debug;
 }
 void ArduinoPPM::send(int values[]){
     for(int i=0;i<sizeof(values);i++){
         int value=values[i];
         digitalWrite(_pin, LOW);
-        #ifdef debug
+        if(_debug){
             Serial.print(0);
-        #endif
+        }
         delayMicroseconds(400);
         digitalWrite(_pin, HIGH);
-        #ifdef debug
+        if(_debug){
             Serial.print(1);
-        #endif
+        }
         delayMicroseconds(value);
     }
-    #ifdef debug
+    if(_debug){
         Serial.print("Wait");
-    #endif
+    }
     delayMicroseconds(12100);
 }
